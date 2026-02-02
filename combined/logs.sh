0cd095f0a1ea549f42eb34470a9361a88d8f7408
TusharKarsan <TusharKarsan@hotmail.com>
2026-02-02 18:19:50 +0000

    fix script files

---
+++ combined/logs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export BASE_DIR=/home/tushar/data
+cd /home/tushar/data
+docker compose logs -f comfyui
--- combined/services-up.sh
+++ combined/services-up.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 export BASE_DIR=/home/tushar/data
 cd /home/tushar/settings/combined
 # docker compose build --no-cache
