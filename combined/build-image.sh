f963ffbccf8654f941f9322da255ce00beef24f2
TusharKarsan <TusharKarsan@hotmail.com>
2026-02-07 09:00:04 +0000

    Build script

---
+++ combined/build-image.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export BASE_DIR=/home/tushar/data
+cd /home/tushar/settings/combined
+docker compose build --no-cache comfyui
+# docker compose build --no-cache mcp-bridge
+docker system prune -f
+docker builder prune -a -f
+cd /home/tushar
