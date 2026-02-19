662bdd1e3db9c0a9d86afe4e5a51f1adb6f1ca8b
TusharKarsan <TusharKarsan@hotmail.com>
2026-02-19 05:02:56 +0000

    fix

--- combined/build-image.sh
+++ combined/build-image.sh
@@ -5,6 +5,7 @@ cd /home/tushar/settings/combined
 # docker compose pull
 # docker compose build --no-cache comfyui
 # docker compose build --no-cache mcp-bridge
+docker compose pull
 docker compose build --no-cache --pull
 docker builder prune -a -f
 # docker system prune -f
