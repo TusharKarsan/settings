1dddb029f6838d24dc8c4060332815a08b929d55
TusharKarsan <TusharKarsan@hotmail.com>
2026-02-19 01:32:06 +0000

    update build script

--- combined/build-image.sh
+++ combined/build-image.sh
@@ -2,8 +2,10 @@
 
 export BASE_DIR=/home/tushar/data
 cd /home/tushar/settings/combined
-docker compose build --no-cache comfyui
+# docker compose pull
+# docker compose build --no-cache comfyui
 # docker compose build --no-cache mcp-bridge
+docker compose build --no-cache --pull
 docker system prune -f
 docker builder prune -a -f
 # cd /home/tushar
