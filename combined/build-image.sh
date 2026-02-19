614dcd347e46c08744570b4add96e6952f97ec77
TusharKarsan <TusharKarsan@hotmail.com>
2026-02-19 01:34:30 +0000

    Optimise

--- combined/build-image.sh
+++ combined/build-image.sh
@@ -6,6 +6,6 @@ cd /home/tushar/settings/combined
 # docker compose build --no-cache comfyui
 # docker compose build --no-cache mcp-bridge
 docker compose build --no-cache --pull
-docker system prune -f
+# docker system prune -f
 docker builder prune -a -f
 # cd /home/tushar
