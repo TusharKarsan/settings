d3682b8d4546ba7089e10a583b753edec9ae368a
TusharKarsan <tusharkarsan@hotmail.com>
2026-04-26 20:43:38 +0100

    Ready to test lab-code-02

---
+++ lab-code-02/services-down.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export BASE_DIR=/home/tushar/data
+cd /home/tushar/settings/lab-code-02
+docker compose down --remove-orphans
+cd /home/tushar
---
+++ lab-code-02/services-up.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export BASE_DIR=/home/tushar/data
+cd /home/tushar/settings/lab-code-02
+docker compose up -d
+cd /home/tushar
